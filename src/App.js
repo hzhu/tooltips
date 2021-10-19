bbfcc4ebbd23e51ac2e487f5860b4653bc04cb1d
hzhu <hz@henryzhu.me>
2021-10-19 10:39:54 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,161 @@
-import logo from './logo.svg';
-import './App.css';
+import { Tooltip } from "@zendeskgarden/react-tooltips";
+import { DEFAULT_THEME, ThemeProvider } from "@zendeskgarden/react-theming";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={DEFAULT_THEME}>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+      <Tooltip content="Eat, drink, and be rosemary">
+        <button>Hover for a tooltip</button>
+      </Tooltip>
+    </ThemeProvider>
   );
 }
 
